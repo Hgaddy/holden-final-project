a2c6fe6116d3108c3d4f496b7d60e4d7629bc045
Hgaddy <holdengaddy@gmail.com>
2022-05-06 16:15:06 -0500

    Added StartState basics.

---
+++ StartState.hx
@@ -0,0 +1,50 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.system.frontEnds.HTML5FrontEnd.FlxIOSDevice;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+class StartState extends FlxState
+{
+	var titleText:FlxText;
+	var nextText:FlxText;
+
+	override public function create()
+	{
+		if (FlxG.sound.music == null) // don't restart the music if it's already playing
+		{
+			// Start music
+			// FlxG.sound.playMusic(AssetPaths., 0.8, true);
+		}
+
+		// Create title
+		titleText = new FlxText(20, 0, 0, "GameName", 35);
+		titleText.screenCenter(X);
+		add(titleText);
+
+		// Create guidance text
+		nextText = new FlxText(20, 0, 0, "Press Start", 20);
+		nextText.screenCenter(XY);
+		add(nextText);
+	}
+
+	override public function update(elapsed:Float)
+	{
+		// Set fullscreen
+		if (FlxG.gamepads.anyJustPressed(BACK))
+		{
+			FlxG.fullscreen = !FlxG.fullscreen;
+		}
+		// Load next state
+		if (FlxG.gamepads.anyJustPressed(START))
+		{
+			FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
+			{
+				FlxG.sound.music.stop();
+				FlxG.switchState(new PlayState());
+			});
+		}
+	}
+}
